e2a90a2f20cc4828e51faf3309d1574a9fc4df8d
Your Name <your-email@example.com>
2026-05-27 16:01:59 +0545

    prepare backend for render

--- backend/server.js
+++ backend/server.js
@@ -58,7 +58,13 @@ app.post("/bookings", async (req, res) => {
 app.listen(5000, () => {
   console.log("Server running on port 5000");
 });
+
+
+
 const PORT = process.env.PORT || 5000;
+
 app.listen(PORT, () => {
-  console.log("Server running");
+  console.log("Server running on port", PORT);
 });
+
+process.env.MONGO_URL
